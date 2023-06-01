ddb6b7d38c3f6a9528677afb6583f82478b4d690
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2023-06-01 21:12:45 +0700

    Update AdaptiveFuzzyPID.cpp

--- src/AdaptiveFuzzyPID.cpp
+++ src/AdaptiveFuzzyPID.cpp
@@ -4,5 +4,30 @@
 #include "AdaptiveFuzzyPID.h"
 
 AdaptiveFuzzyPID::AdaptiveFuzzyPID() {
+    this->sampleTimeInMs = 100;
     
+    this->previousMillis = millis() - this->sampleTimeInMs;
+}
+
+long AdaptiveFuzzyPID::getSampleTime(void) {
+    return this->sampleTimeInMs;
+}
+
+void AdaptiveFuzzyPID::setSampleTime(unsigned long sampleTime) {
+    this->sampleTimeInMs = sampleTime;
+}
+
+void AdaptiveFuzzyPID::setMembershipFunctionType(MembershipFunctionType type) {
+    this->currentMembershipFunctionType = type;
+}
+
+void AdaptiveFuzzyPID::update(double setpoint, double input) {
+    this->currentMillis = millis();
+
+    if (this->currentMillis - this->previousMillis >= sampleTimeInMs) {
+        this->previousMillis = this->currentMillis;
+
+        double error = setpoint - input;
+
+    }
 }
